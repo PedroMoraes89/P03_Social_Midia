be3ba16b12e898adce9ddb555b0b24dca00e2f99
PEDRO HENRIQUE DE OLIVEIRA MORAES <86898701+PedroMoraes89@users.noreply.github.com>
2022-10-26 11:13:38 -0700

    Added the code to share images

--- P01 - Storm Viewer/DetailViewController.swift
+++ P01 - Storm Viewer/DetailViewController.swift
@@ -14,24 +14,36 @@ class DetailViewController: UIViewController {
     var selectedImage: String?
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        title = selectedImage
-        navigationItem.largeTitleDisplayMode = .never
+            super.viewDidLoad()
+            
+            title = selectedImage
+            navigationItem.largeTitleDisplayMode = .never
+            
+            navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(sharedTapped))
 
-        if let imageToLoad = selectedImage {
-            imageView.image = UIImage(named: imageToLoad)
+            if let imageToLoad = selectedImage {
+                imageView.image = UIImage(named: imageToLoad)
+            }
         }
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.hidesBarsOnTap = true
-    }
+        
+        override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            navigationController?.hidesBarsOnTap = true
+        }
 
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        navigationController?.hidesBarsOnTap = false
-    }
+        override func viewWillDisappear(_ animated: Bool) {
+            super.viewWillDisappear(animated)
+            navigationController?.hidesBarsOnTap = false
+        }
+        
+        @objc func sharedTapped() {
+            guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+                print("No image found")
+                return
+            }
+            let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+            vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+            present(vc, animated: true)
+        }
 
-}
+    }
